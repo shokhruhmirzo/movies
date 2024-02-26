b5ed4fe2d64b532015d21bf5e4e31019c988b0dd
shokhruhmirzo <shaalimov923@gmail.com>
2024-02-26 20:46:00 +0500

    add links code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+// javascript codes
 let partMovies = movies.slice(0, 99)
 let elList = document.querySelector('.movies__list')
 let elCategory = document.querySelector('.js__category')
+let elImg = document.querySelector('.white__heart')
 dataRender(partMovies)
 
-function dataRender(data){
+console.log(elImg);
+function dataRender(data) {
     elList.innerHTML = ''
     data.forEach((item, index) => {
         let newItem = document.createElement('li')
@@ -12,50 +15,65 @@ function dataRender(data){
         <a href="https://www.youtube.com/watch?v=${item.ytid}" target="_blank" >
         <img src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg" alt="">
         </a>
+        <div class="js__flex">
         <p class="movies__text">${item.movie_year}</p>
+        <img onclick="img('${item.ytid}')" class="white__heart" src="./img/white__heart.svg" alt="">
+        </div>
         <h3 class="movies__h3">${item.Title}</h3>
         <p class="categories">${item.Categories}</p>
         <p class="app__rating">${item.imdb_rating}</p>
     </li>
         `
-    
         elList.appendChild(newItem)
     })
 }
-function fnYear(value){
-    if(value == 'old'){
-        dataRender(partMovies.sort((a,b)=> a.movie_year - b.movie_year));
-    }else{
-        dataRender(partMovies.sort((a,b)=> b.movie_year - a.movie_year));
+function fnYear(value) {
+    if (value == 'old') {
+        dataRender(partMovies.sort((a, b) => a.movie_year - b.movie_year));
+    } else {
+        dataRender(partMovies.sort((a, b) => b.movie_year - a.movie_year));
 
     }
 }
 
-function fnReating(value){
-    if(value == 'min'){
-        dataRender(partMovies.sort((a,b)=> a.imdb_rating - b.imdb_rating));
-    }else{
-        dataRender(partMovies.sort((a,b)=> b.imdb_rating - a.imdb_rating));
+function fnReating(value) {
+    // created by shoxrux
+    if (value == 'min') {
+        dataRender(partMovies.sort((a, b) => a.imdb_rating - b.imdb_rating));
+    } else {
+        dataRender(partMovies.sort((a, b) => b.imdb_rating - a.imdb_rating));
     }
 }
 
 let arrCategory = []
-partMovies.forEach((item)=>{
-    if(!arrCategory.includes(item.Categories)){
+partMovies.forEach((item) => {
+    if (!arrCategory.includes(item.Categories)) {
         arrCategory.push(item.Categories)
     }
 })
-
-// created by shoxrux
-
-arrCategory.forEach(item=>{
+arrCategory.forEach(item => {
     let newOption = document.createElement('option')
     newOption.textContent = item
     newOption.value = item
     elCategory.appendChild(newOption)
 })
 
-function fnCategories(value){
-    dataRender(partMovies.filter((item)=> item.Categories == value));
+function fnCategories(value) {
+    dataRender(partMovies.filter((item) => item.Categories == value));
 }
 
+function search(e) {
+    e.preventDefault()
+    let mov = e.target.mov.value
+    dataRender(partMovies.filter((i) =>
+        i.Title.toString().toLowerCase().includes(mov.toLowerCase()) &&
+        i.Title.toString().toLowerCase()[0] == mov.toLowerCase()[0]
+    ))
+};
+
+var intialLocalData = []
+function img(id) {
+    console.log(partMovies.find((item) => item.ytid == id));
+    intialLocalData.push(partMovies.find((item) => item.ytid == id))
+    window.localStorage.setItem('localMovie', JSON.stringify(intialLocalData))
+}
